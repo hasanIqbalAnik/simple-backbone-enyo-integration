ddf692f512c07dcc499116f5f268e26bde85f3fc
hasan.iqbal <hasan.iqbal@tasawr.com>
2014-04-29 19:08:13 +0600

    namespace added

--- source/views/ApplicationView.js
+++ source/views/ApplicationView.js
@@ -15,9 +15,9 @@ enyo.kind({
 
     addProductToList: function (inSender, inEvent) {
         if (inEvent.keyCode == 13) {
-            getAll(this.buildList);
+            Db.getAll(this.buildList);
             var product = new Product({name: inSender.value});
-            this.addProductToDb(inSender.value);
+//            this.addProductToDb(inSender.value);
             this.$.productListView.productList.add(product);
             this.$.productListView.productListChanged();
             inSender.setValue("");
@@ -33,16 +33,16 @@ enyo.kind({
         }
     },
     addProductToDb: function (productName) {
-        add(productName);
+        Db.add(productName);
     },
     buildList: function (results) {
-        _.each(
-            results.rows,
-            function (rowIndex) {
-                var row = results.rows.item(rowIndex);
-                console.log(row.name);
-            }
-        );
+//        _.each(
+//            results.rows,
+//            function (rowIndex) {
+//                var row = results.rows.item(rowIndex);
+//                console.log(row.name);
+//            }
+//        );
     }
 
 });
--- source/webSqlDB/crud.js
+++ source/webSqlDB/crud.js
@@ -1,60 +1,77 @@
 /**
  * Created by anik on 4/29/14.
  */
-var add = function (productName, callback) {
-    // Insert a new girl.
-    database.transaction(
-        function (transaction) {
-            transaction.executeSql(
-                (
-                    "INSERT INTO product (" +
-                        "name " +
-                        " ) VALUES ( " +
-                        "? " +
-                        ");"
-                    ),
-                [
-                    productName
-                ],
-                function (transaction, results) {
-                    console.log('product inserted');
-                    if (callback) {
+
+(function () {
+    Db = window.Db || {}
+
+    Db.add = function (productName, callback) {
+        // Insert a new girl.
+        database.transaction(
+            function (transaction) {
+                transaction.executeSql(
+                    (
+                        "INSERT INTO product (" +
+                            "name " +
+                            " ) VALUES ( " +
+                            "? " +
+                            ");"
+                        ),
+                    [
+                        productName
+                    ],
+                    function (transaction, results) {
+                        console.log('product inserted');
+                        if (callback) {
+                            callback(results);
+                        }
+                    }
+                );
+            }
+        );
+    };
+    Db.remove = function (callback) {
+        database.transaction(
+            function (transaction) {
+                transaction.executeSql(
+                    (
+                        "DELETE FROM " +
+                            "product "
+                        ),
+                    [],
+                    function (transaction, results) {
+                        console.log('products removed');
+//                    callback();
+                    }
+                );
+
+            }
+        );
+    };
+
+    Db.getAll = function (callback) {
+        database.transaction(
+            function (transaction) {
+                transaction.executeSql(
+                    (
+                        "SELECT " +
+                            "* " +
+                            "FROM " +
+                            "product " +
+                            "ORDER BY " +
+                            "name ASC"
+                        ),
+                    [],
+                    function (transaction, results) {
+                        console.log(transaction);
+                        console.log(results);
                         callback(results);
                     }
-                }
-            );
-        }
-    );
-};
-var remove = function (callback) {
-    database.transaction(
-        function (transaction) {
-            transaction.executeSql(
-                (
-                    "DELETE FROM " +
-                        "product "
-                    ),
-                [],
-                function (transaction, results) {
-                    console.log('products removed');
-                    callback();
-                }
-            );
+                );
 
-        }
-    );
-};
+            }
+        );
+    };
 
-var getAll = function (callback) {
-    database.transaction(function (transaction) {
-        transaction.executeSql(
-            (
-                "select * from product"
-                ),
-            [],
-            function (transaction, results) {
-                callback(results);
-                console.log('products fetched')
-            });
-    });
-};
+}());
+
